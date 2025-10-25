3d69cfc5792e79f5d28e3b328ccfc74767a6fe07
Mizu545 <quattlebaum.raven@gmail.com>
2025-10-25 10:31:06 -0400

    Adding Light

--- Assets/VRM10/Mizutoon_input.hlsl
+++ Assets/VRM10/Mizutoon_input.hlsl
@@ -1,20 +1,20 @@
 #ifndef VRMC_MATERIALS_MTOON_INPUT_INCLUDED
 #define VRMC_MATERIALS_MTOON_INPUT_INCLUDED
 
-#include "./vrmc_materials_mtoon_render_pipeline.hlsl"
+#include "./Mizutoon_pipeline.hlsl"
 
 // Textures
-MTOON_DECLARE_TEX2D(_MainTex);
-MTOON_DECLARE_TEX2D(_LightTex);
-MTOON_DECLARE_TEX2D(_ShadeTex);
-MTOON_DECLARE_TEX2D(_BumpMap);
-MTOON_DECLARE_TEX2D(_ShadingShiftTex);
-MTOON_DECLARE_TEX2D(_EmissionMap);
-MTOON_DECLARE_TEX2D(_MatcapTex);
-MTOON_DECLARE_TEX2D(_RimTex);
-MTOON_DECLARE_TEX2D(_OutlineWidthTex);
+MIZUTOON_DECLARE_TEX2D(_MainTex);
+MIZUTOON_DECLARE_TEX2D(_LightTex);
+MIZUTOON_DECLARE_TEX2D(_ShadeTex);
+MIZUTOON_DECLARE_TEX2D(_BumpMap);
+MIZUTOON_DECLARE_TEX2D(_ShadingShiftTex);
+MIZUTOON_DECLARE_TEX2D(_EmissionMap);
+MIZUTOON_DECLARE_TEX2D(_MatcapTex);
+MIZUTOON_DECLARE_TEX2D(_RimTex);
+MIZUTOON_DECLARE_TEX2D(_OutlineWidthTex);
 // NOTE: "tex2d() * _Time.y" returns mediump value if sampler is half precision in Android VR platform
-MTOON_DECLARE_TEX2D_FLOAT(_UvAnimMaskTex);
+MIZUTOON_DECLARE_TEX2D_FLOAT(_UvAnimMaskTex);
 
 CBUFFER_START(UnityPerMaterial)
 // Vector
